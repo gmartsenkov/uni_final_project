4b378f141ac679dbd8d8b849625d0f5067e88f60
Georgi Martsenkov <g.martsenkov@gmail.com>
2020-11-09 14:23:16 +0000

    conference csv export service

---
+++ lib/uni/exports/conferences.ex
@@ -0,0 +1,28 @@
+defmodule Uni.Exports.Conferences do
+  @columns ["Създател", "Име", "Страници", "Тип", "Докладван", "Публикуван"]
+
+  def call(conferences) do
+    conferences
+    |> Enum.map(&row/1)
+    |> List.insert_at(0, @columns)
+    |> CSV.encode()
+    |> Enum.into("")
+  end
+
+  defp row(conference) do
+    [
+      conference.owner.name,
+      conference.name,
+      "#{conference.page_start} - #{conference.page_end}",
+      type(conference.type),
+      bool(conference.reported),
+      bool(conference.published),
+    ]
+  end
+
+  def bool(true), do: "Да"
+  def bool(false), do: "Не"
+
+  def type("national"), do: "Национален"
+  def type("international"), do: "Международен"
+end
---
+++ test/uni/exports/conferences_test.exs
@@ -0,0 +1,44 @@
+defmodule Uni.Exports.ConferencesTest do
+  use ExSpec
+
+  alias Uni.Conferences.Conference
+  alias Uni.Users.User
+  alias Uni.Exports.Conferences, as: Export
+
+  describe "when no conferences" do
+    test "it returns the correct csv string" do
+      assert Export.call([]) == "Създател,Име,Страници,Тип,Докладван,Публикуван\r\n"
+    end
+  end
+
+  describe "with conferences" do
+    test "it returns the correct csv string" do
+      authors = [
+        %Conference{
+          name: "Web Conf",
+          page_start: 10,
+          page_end: 20,
+          type: "national",
+          reported: true,
+          published: false,
+          owner: %User{name: "Bob"}
+        },
+        %Conference{
+          name: "Mobile Conf",
+          page_start: 15,
+          page_end: 25,
+          type: "international",
+          reported: true,
+          published: false,
+          owner: %User{name: "Jon"}
+        }
+      ]
+
+      assert Export.call(authors) == ~s"""
+      Създател,Име,Страници,Тип,Докладван,Публикуван\r
+      Bob,Web Conf,10 - 20,Национален,Да,Не\r
+      Jon,Mobile Conf,15 - 25,Международен,Да,Не\r
+      """
+    end
+  end
+end
