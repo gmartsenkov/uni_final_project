a5769c6105dbb9d06b439f793bb576990ad0b586
Georgi Martsenkov <g.martsenkov@gmail.com>
2020-11-13 19:56:33 +0000

    add utf-8 bom for csv export (so that excel will set the correct utf-8 encoding)

--- lib/uni_web/controllers/exports.ex
+++ lib/uni_web/controllers/exports.ex
@@ -1,6 +1,8 @@
 defmodule UniWeb.ExportsController do
   use UniWeb, :controller
 
+  @bom :unicode.encoding_to_bom(:utf8)
+
   alias Uni.Articles.Article
   alias Uni.Articles
   alias Uni.Monographs.Monograph
@@ -15,50 +17,62 @@ defmodule UniWeb.ExportsController do
   alias Uni.Exports.Conferences, as: ConferenceExporter
 
   def articles(conn, params) do
-    csv = Article
-    |> Articles.filter(Map.to_list(params))
-    |> Articles.graph()
-    |> ArticleExporter.call()
+    csv =
+      Article
+      |> Articles.filter(Map.to_list(params))
+      |> Articles.graph()
+      |> ArticleExporter.call()
 
-    conn
-    |> put_resp_content_type("text/csv")
-    |> put_resp_header("content-disposition", "attachment; filename=\"articles.csv\"")
-    |> send_resp(200, csv)
+    send_download(
+      conn,
+      {:binary, Enum.join([@bom, csv])},
+      content_type: "application/csv; charset=utf-8",
+      filename: "articles.csv"
+    )
   end
 
   def monographs(conn, params) do
-    csv = Monograph
-    |> Monographs.filter(Map.to_list(params))
-    |> Monographs.graph()
-    |> MonographExporter.call()
+    csv =
+      Monograph
+      |> Monographs.filter(Map.to_list(params))
+      |> Monographs.graph()
+      |> MonographExporter.call()
 
-    conn
-    |> put_resp_content_type("text/csv")
-    |> put_resp_header("content-disposition", "attachment; filename=\"monographs.csv\"")
-    |> send_resp(200, csv)
+    send_download(
+      conn,
+      {:binary, Enum.join([@bom, csv])},
+      content_type: "application/csv; charset=utf-8",
+      filename: "monographs.csv"
+    )
   end
 
   def projects(conn, params) do
-    csv = Project
-    |> Projects.filter(Map.to_list(params))
-    |> Projects.graph()
-    |> ProjectExporter.call()
+    csv =
+      Project
+      |> Projects.filter(Map.to_list(params))
+      |> Projects.graph()
+      |> ProjectExporter.call()
 
-    conn
-    |> put_resp_content_type("text/csv")
-    |> put_resp_header("content-disposition", "attachment; filename=\"projects.csv\"")
-    |> send_resp(200, csv)
+    send_download(
+      conn,
+      {:binary, Enum.join([@bom, csv])},
+      content_type: "application/csv; charset=utf-8",
+      filename: "projects.csv"
+    )
   end
 
   def conferences(conn, params) do
-    csv = Conference
-    |> Conferences.filter(Map.to_list(params))
-    |> Conferences.graph()
-    |> ConferenceExporter.call()
+    csv =
+      Conference
+      |> Conferences.filter(Map.to_list(params))
+      |> Conferences.graph()
+      |> ConferenceExporter.call()
 
-    conn
-    |> put_resp_content_type("text/csv")
-    |> put_resp_header("content-disposition", "attachment; filename=\"conferences.csv\"")
-    |> send_resp(200, csv)
+    send_download(
+      conn,
+      {:binary, Enum.join([@bom, csv])},
+      content_type: "application/csv; charset=utf-8",
+      filename: "conferences.csv"
+    )
   end
 end
