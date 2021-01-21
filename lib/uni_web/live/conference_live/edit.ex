1df4c84a44d194d6f3b33e1b8ef7dab31bf1ebb4
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-01-21 15:11:11 +0000

    ability to delete conferences

--- lib/uni_web/live/conference_live/edit.ex
+++ lib/uni_web/live/conference_live/edit.ex
@@ -14,6 +14,22 @@ defmodule UniWeb.ConferenceLive.Edit do
     {:noreply, maybe_get_conference(socket, Conferences.get_conference(id))}
   end
 
+  @impl true
+  def handle_event("delete", _params, socket) do
+    case Conferences.delete_conference(socket.assigns.conference) do
+      {:ok, _conference} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, gettext("Conference deleted successfuly"))
+         |> push_redirect(to: Routes.conference_index_path(socket, :conferences))}
+      {:error, _changeset} ->
+        {:noreply,
+         socket
+         |> put_flash(:error, gettext("An error occured"))
+         |> push_redirect(to: Routes.conference_edit_path(socket, :conferences, socket.assigns.conference.id))}
+    end
+  end
+
   defp maybe_get_conference(socket, nil = _conference) do
     socket
     |> put_flash(:error, gettext("Conference not found"))
