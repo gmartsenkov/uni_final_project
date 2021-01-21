e66b2c8357483e5acb80a4d12c5e783953a33820
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-01-21 15:09:52 +0000

    ability to delete a project

--- lib/uni_web/live/project_live/edit.ex
+++ lib/uni_web/live/project_live/edit.ex
@@ -14,6 +14,22 @@ defmodule UniWeb.ProjectLive.Edit do
     {:noreply, maybe_get_project(socket, Projects.get_project(id))}
   end
 
+  @impl true
+  def handle_event("delete", _params, socket) do
+    case Projects.delete_project(socket.assigns.project) do
+      {:ok, _project} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, gettext("Project deleted successfuly"))
+         |> push_redirect(to: Routes.project_index_path(socket, :projects))}
+      {:error, _changeset} ->
+        {:noreply,
+         socket
+         |> put_flash(:error, gettext("An error occured"))
+         |> push_redirect(to: Routes.project_edit_path(socket, :projects, socket.assigns.project.id))}
+    end
+  end
+
   defp maybe_get_project(socket, nil = _project) do
     socket
     |> put_flash(:error, gettext("Project not found"))
